f708d6891e13c83405ef9a88e3710633710e9f37
Kay Stenschke <info@stenschke.com>
2014-05-18 12:09:04 +0200

    Improved declensions, extended vocabulary and sentence vicissitude

--- src/com/kstenschke/dummytext/dictionaries/DictionaryCookery.java
+++ src/com/kstenschke/dummytext/dictionaries/DictionaryCookery.java
@@ -64,21 +64,26 @@ public class DictionaryCookery extends Dictionary {
 
 			while(sentence.contains("RNDNUM")) sentence = sentence.replaceFirst("RNDNUM", getNumber() );
 			while(sentence.contains("HEATUP")) sentence = sentence.replaceFirst("HEATUP", getVerbHeatUp() );
+			while(sentence.contains("MANIPULATE")) sentence = sentence.replaceFirst("MANIPULATE", getVerbManipulate() );
+			while(sentence.contains("CHARACTERISTIC")) sentence = sentence.replaceFirst("CHARACTERISTIC", getNounCharacteristic() );
 		}
 
 		sentence = InflectionHelper.fixIndefiniteArticles(sentence);
 
-		sentence = sentence.replaceAll("chickens",      "chicken");
 		sentence = sentence.replaceAll(" beefs",        " beef ");
-		sentence = sentence.replaceAll(" cuted ",       " cut ");
-		sentence = sentence.replaceAll("shreding",      "shredding");
-		sentence = sentence.replaceAll("spinachs",      "spinach");
-		sentence = sentence.replaceAll("sauce soup",    "soup");
-		sentence = sentence.replaceAll(" choping ",     " chopping ");
-		sentence = sentence.replaceAll(" jumbleed ",     " jumbled ");
 		sentence = sentence.replaceAll(" butters ",     " butter ");
-		sentence = sentence.replaceAll(" scrapeed ",     " scraped ");
+		sentence = sentence.replaceAll(" caviars ",     " caviar ");
+		sentence = sentence.replaceAll(" chickens ",    " chicken ");
+		sentence = sentence.replaceAll(" choping ",     " chopping ");
+		sentence = sentence.replaceAll(" cuted ",       " cut ");
+		sentence = sentence.replaceAll(" jumbleed ",    " jumbled ");
+		sentence = sentence.replaceAll(" mash uped ",   " mashed up ");
+		sentence = sentence.replaceAll(" rubing ",      " rubbing ");
+		sentence = sentence.replaceAll(" sauce soup ",  " soup ");
+		sentence = sentence.replaceAll(" scrapeed ",    " scraped ");
 		sentence = sentence.replaceAll(" shakeed ",     " shaken ");
+		sentence = sentence.replaceAll(" shreding ",    " shredding ");
+		sentence = sentence.replaceAll(" spinachs ",    " spinach ");
 
 		String[] unincreasables   = {
 			"beefs", "berries", "breasts", "carrots", "chicken", "chicken", "chickpeas", "crumps", "eggs", "lentils",
@@ -101,31 +106,33 @@ public class DictionaryCookery extends Dictionary {
 		}
 
 		String[] structures  = {
-            "4 RNDNUM PLURAL1, 1, and 7 in a large 8 over medium heat, HEATUP for RNDNUM minutes and 3 with some 1.",
-            "combine 1, 1 and 1. 3 with 5 7 and serve PASTTENSE4 with 1. Enjoy!",
-            "3 the 1 with 5 7, 7, 7, and 7 making sure to cover all of it.",
-            "When GERUND4 5 PLURAL1, be sure they are room temperature.",
-            "Remember: 4ed 1 tastes best when 4ed in a 8 3ed with 7.",
-            "After GERUND4 the PLURAL1, 3 1, 1 and 2 with it in a 8.",
-            "per guest prepare 9 of 2 with PASTTENSE4 1 for dessert.",
-            "2 soup is just not the same without 7 and 5 5 PLURAL1.",
-            "1 can be replaced by 5 1, than try 3ing the 0 with 2.",
-            "Try GERUND3 the 2 1s with 5 2 and 2, PASTTENSEHEATUP.",
-            "What’s the secret to 5 and 5 1? Always use 5 7.",
-            "Instead of GERUND3 5 2 with 1, use 9 2 and 9 7 8.",
-            "All children like PASTTENSE4 PLURAL1 in 2 and 7.",
-            "5, 5 pudding is best PASTTENSE3 with 5 2.",
-            "4 1 6, then mix with 2 and serve 6 in 8.",
-            "Place the 1 in a 8, and 3 6 with 5 2.",
-            "5 1 can be made 5 by GERUND3 with 2.",
-            "1 0 has to have a 5, 5 1 component.",
-            "1 tastes best with 2 and lots of 7.",
-            "try 4ing 0 3ed with 2, 3ed with 7.",
-            "3 each side of the 1 with 9 of 1.",
-            "To the 5 1 add 1, 1, 2 and 5 1.",
-            "Try 4ing 0 3ed with 2.",
-            "With PLURAL1 drink 2.",
-            "3 9 of 1 in 9 of 2.",
+                "4 RNDNUM PLURAL1, 1, and 7 in a large 8 over medium heat, HEATUP for RNDNUM minutes and 3 with some 1.",
+                "HEATUP 5 1s in a 8 with 2 for about an hour to MANIPULATE their CHARACTERISTIC.",
+                "combine 1, 1 and 1. 3 with 5 7 and serve PASTTENSE4 with 1. Enjoy!",
+                "3 the 1 with 5 7, 7, 7, and 7 making sure to cover all of it.",
+                "When GERUND4 5 PLURAL1, be sure they are room temperature.",
+                "Remember: 4ed 1 tastes best when 4ed in a 8 3ed with 7.",
+                "After GERUND4 the PLURAL1, 3 1, 1 and 2 with it in a 8.",
+                "per guest prepare 9 of 2 with PASTTENSE4 1 for dessert.",
+                "2 soup is just not the same without 7 and 5 5 PLURAL1.",
+                "1 can be replaced by 5 1, than try 3ing the 0 with 2.",
+                "Try GERUND3 the 2 1s with 5 2 and 2, PASTTENSEHEATUP.",
+                "What’s the secret to 5 and 5 1? Always use 5 7.",
+                "Instead of GERUND3 5 2 with 1, use 9 2 and 9 7 8.",
+                "All children like PASTTENSE4 PLURAL1 in 2 and 7.",
+                "5, 5 pudding is best PASTTENSE3 with 5 2.",
+                "4 1 6, then mix with 2 and serve 6 in 8.",
+                "Place the 1 in a 8, and 3 6 with 5 2.",
+                "5 1 can be made 5 by GERUND3 with 2.",
+                "1 0 has to have a 5, 5 1 component.",
+                "1 tastes best with 2 and lots of 7.",
+                "try 4ing 0 3ed with 2, 3ed with 7.",
+                "3 each side of the 1 with 9 of 1.",
+                "To the 5 1 add 1, 1, 2 and 5 1.",
+                "For a 5 5 0, add some 2 and 7.",
+                "Try 4ing 1 0 3ed with 2.",
+                "With PLURAL1 drink 2.",
+                "3 9 of 1 in 9 of 2.",
 		};
 
 		return pickRandomString(structures, amountWords);
@@ -170,11 +177,11 @@ public class DictionaryCookery extends Dictionary {
 		String[] words ={
 			"adobo sauce", "anchovy essence", "BBQ sauce", "beer", "bourbon", "buttermilk", "champaign",
             "cocktail sauce", "coconut milk", "coffee", "condensed milk", "cream", "crême fraîche", "emeril\'s essence",
-            "fish sauce", "gold tequila", "gravy", "hollandaise sauce", "honey", "iced tea", "joghurt", "kefir", "ketchup",
-            "lemon juice", "lime", "maple syrup", "mayonnaise", "milk", "mint sauce", "olive oil", "orange juice",
-            "oyster sauce", "peanut sauce", "peppermint tea", "plain vinegar", "red wine", "remoulade", "rice vinegar",
-            "ricotta", "rum", "salad cream", "salsa verde", "sour milk", "soy sauce", "sweet chili sauce", "tabasco",
-            "tea", "teriyaki", "triple sec", "vinaigrette", "vinegar", "water", "whipped cream", "whiskey",
+            "fish sauce", "gold tequila", "gravy", "hollandaise sauce", "honey", "iced tea", "ice water", "joghurt",
+            "kefir", "ketchup", "lemon juice", "lime", "maple syrup", "mayonnaise", "milk", "mint sauce", "olive oil",
+            "orange juice", "oyster sauce", "peanut sauce", "peppermint tea", "plain vinegar", "red wine", "remoulade",
+            "rice vinegar", "ricotta", "rum", "salad cream", "salsa verde", "sour milk", "soy sauce", "sweet chili sauce",
+            "tabasco", "tea", "teriyaki", "triple sec", "vinaigrette", "vinegar", "water", "whipped cream", "whiskey",
             "white wine", "worcestershire sauce"
 		};
 
@@ -217,18 +224,29 @@ public class DictionaryCookery extends Dictionary {
 		return pickRandomString(words);
 	}
 
+    /**
+     * @return  Word of group MANIPULATE - manipulation word (e.g. "to cut the bitterness")
+     */
+    private static String getVerbManipulate() {
+        String[] words ={
+                "bring down", "chamfer", "cut", "enhance", "increase", "milden", "perfect", "soothe", "upgrade"
+        };
+
+        return pickRandomString(words);
+    }
+
 	/**
 	 * @return  Word of group 5:  adjective
 	 */
 	private static String getAdjective() {
 		String[] words = {
-			"aged", "al dente", "aromatic", "bloody", "canned", "chilled", "chopped", "clammy", "cold", "cored",
-            "crushed", "crusted", "dark", "delicious", "diced", "divided", "dried", "fluffy", "fresh", "gooey", "grey",
-            "ground", "hardened", "heated", "hot", "iced", "instant", "juicy", "large", "large", "melted", "mild",
-            "minced", "muddy", "niffy", "nutty", "old", "packaged", "puréed", "quartered", "raw", "rich", "ripe",
+			"aged", "al dente", "aromatic", "bitter", "bloody", "canned", "chilled", "chopped", "clammy", "cold",
+            "cored", "crushed", "crusted", "dark", "delicious", "diced", "divided", "dried", "fluffy", "fresh", "gooey",
+            "grey", "ground", "hardened", "heated", "hot", "iced", "instant", "juicy", "large", "large", "melted",
+            "mild", "minced", "muddy", "niffy", "nutty", "old", "packaged", "puréed", "quartered", "raw", "rich", "ripe",
             "roasted", "salted", "salty", "shredded", "sichuan-style", "sliced", "slobbery", "small", "smashed",
-            "smooth", "soaked", "sour", "sour", "springy", "squeezed", "sticky", "sun-dried", "sweet", "synthetic",
-            "tangy", "tasty", "tender", "thin", "warm", "whole", "yellow"
+            "smooth", "soaked", "sour", "sour", "springy", "squeezed", "sticky", "sun-dried", "sweet", "tangy", "tasty",
+            "tender", "thin", "warm", "whole", "yellow"
 		};
 
 		return pickRandomString(words);
@@ -293,6 +311,18 @@ public class DictionaryCookery extends Dictionary {
 		String[] words ={
 			"two", "three", "four", "five", "six", "seven", "eight", "nine", "eleven", "twelve", "fifteen", "twenty",
 			"a dozen", "a handfull"
+		};
+
+		return pickRandomString(words);
+	}
+
+	/**
+	 * @return  Characteristic nouns ("how" a food is)
+	 */
+	private static String getNounCharacteristic() {
+		String[] words ={
+			"aroma", "asperity", "bitterness", "consistency", "creaminess", "fierceness", "flavor", "keenness",
+            "mossiness", "pepperiness", "saltyness", "sourness", "sweetness", "tartness", "thickness", "viscosity"
 		};
 
 		return pickRandomString(words);
